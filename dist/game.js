b0b6cfbaa0372736c395f377033d57f4e08a9f97
Nathaniel Felps <ndfelps@gmail.com>
2015-05-13 10:50:15 -0500

    dist/

--- dist/game.js
+++ dist/game.js
@@ -18,7 +18,7 @@ function* chooseYourAdventure() {
 	yield adventure.say('Hello, '+player.myName+"!", "Let\'s get started.")
 	player.myClass = yield adventure.choose(player.myName+", what class would you like to play?", "Knight","Shaman","Rogue","Godmodetest")
 	player.str = 0; /*increases attack damage at base .25 dmg per point*/
-	player.stam = 0; /*increases health at base .25 per point*/
+	player.stam = 0; /*increases health at base .75 per point*/
 	player.inte = 0; /*increases ability dmg at base .75 per point, mana at .5 per point*/
 	player.agi = 0; /*increases dodge chance at base .01 per point*/
 	player.dex = 0; /*increases crit chance at base .01 per point*/
@@ -92,16 +92,77 @@ function* chooseYourAdventure() {
 	if (player.myClass=="Knight") {
 			var myWeap = yield adventure.choose("Choose your equipment, "+player.myClass+"!", "Sword and Shield", "Lance", "Greatsword");
 		} else if (player.myClass=="Shaman") {
-			var myWeap = yield adventure.choose("Choose your equipment, "+player.myClass+"!", "Staff");
+			var myWeap = yield adventure.choose("Choose your equipment, "+player.myClass+"!", "Staff", "Knife and Poisons");
 		} else if (player.myClass=="Rogue") {
-			var myWeap = yield adventure.choose("Choose your equipment, "+player.myClass+"!", "Dueling Rapier and Dagger", "Assassin's Blades", "Bow and Arrow");
+			var myWeap = yield adventure.choose("Choose your equipment, "+player.myClass+"!", "Dueling Rapier and Dagger", "Assassin Blades", "Bow and Arrow");
 		} else if (player.myClass=="Godmodetest") {
-			var myWeap = yield adventure.choose("Choose your equipment, "+player.myClass+"!", "OP");
+			var myWeap = yield adventure.choose("Choose your equipment, "+player.myClass+"!", "OP", "OP");
 		}
-	// var weapons = {
-		
-	// }
-	// player.damage = ((player.str*.25)+weapDam);
+	var myWeap = myWeap.replace(/\s+/, " ");
+	var weapons = {
+		SwordandShield: {
+			weapDam: 9,
+			weapArm: 9,
+			weapInt: 0,
+			weapAgi:-2
+		},
+		Lance: {
+			weapDam: 10,
+			weapArm: 9,
+			weapInt: 0,
+			weapAgi:-3
+		},
+		Greatsword: {
+			weapDam: 16,
+			weapArm: 4,
+			weapInt: 0,
+			weapAgi:-4
+		},
+		Staff: {
+			weapDam: 8,
+			weapArm: 3,
+			weapInt: 6,
+			weapAgi:-1
+		},
+		KnifeandPoisons: {
+			weapDam: 10,
+			weapArm: 0,
+			weapInt: 2,
+			weapAgi: 2
+		},
+		DuelingRapierandDagger: {
+			weapDam: 9,
+			weapArm: 2,
+			weapInt: 0,
+			weapAgi:5
+		},
+		AssassinsBlades: {
+			weapDam: 10,
+			weapArm: 1,
+			weapInt: 0,
+			weapAgi: 5
+		},
+		BowandArrow: {
+			weapDam: 11,
+			weapArm: 0,
+			weapInt: 0,
+			weapAgi: 5
+		},
+		OP: {
+			weapDam: 99,
+			weapArm: 99,
+			weapInt: 99,
+			weapAgi: 99
+		}	
+	}
+	player.damage = ((player.str*.25)+weapDam);
+	player.hitPoints = ((player.stam+*.75))
+	}
+	// 	var newWeap = {};
+	// function weapReplace() {
+	// 	yield adventure.say("Here's your new weapon: "+newWeap, "Stats - Damage: "+newWeap.)
+	// 	myWeap=weapon[myWeap];
+	// } for replacing equipment, not done
 }
 
 
